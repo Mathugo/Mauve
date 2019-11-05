52eb049c1fe452b850cb96dbc2aefb7cec658deb
hugom <hugo.mathh@gmail.com>
2019-11-05 16:00:29 +0100

    correction2

---
+++ src/musicplayer/main.java
@@ -0,0 +1,20 @@
+package musicplayer;
+import java.net.Socket;
+import java.net.UnknownHostException;
+import java.io.BufferedInputStream;
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.net.InetAddress;
+import java.io.IOException;
+
+public class main {
+		public static void main(String[] args)
+		{
+			
+			String host="82.64.162.133";
+			int port=32890;
+			Client cl = new Client(host,port);
+		}
+}		
+
+
